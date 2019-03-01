eac1cce9c53b92ca98986ff4231c524ed912c48f
Amey Jadiye <ameyjadiye@gmail.com>
2019-03-01 23:15:33 +0530

    added result & corrected method name

--- src/main/java/com/codeinventory/jmh_sampling/SamplingEqVsMath.java
+++ src/main/java/com/codeinventory/jmh_sampling/SamplingEqVsMath.java
@@ -4,11 +4,16 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 
+/**
+ * This class checks the avrage running time for sampling if equals method vs math for checking empty strings.
+ * @author Amey
+ *
+ */
 public class SamplingEqVsMath {
 
     @Benchmark
     //@BenchmarkMode(Mode.AverageTime)
-    public void ifTest() {
+    public void eqTest() {
 
         String left = "";
         String right = "";
